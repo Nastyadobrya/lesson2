898c235921ecf4865a9ca345d7db7a9fe1640e57
Anastasia Dobryanskaya <180897_nastya@mail.ru>
2017-06-03 15:35:25 +0300

    Домашняя работа к 2 занятию,доработала

--- bla.py
+++ bla.py
@@ -6,13 +6,16 @@ def  get_answer(ans):
         else:
             ans = str(input('Как дела?  '))
 def  user_ask(q):
-    while True:
-        if q == 'Пока!':
-            print('До скорой встречи!')
-            break
-        else:
-            q = str(input('Сам разберешься с этим! Еще вопросы?'))
+    try:
+        while True:
+            if q == 'Пока!':
+                print('До скорой встречи!')
+                break
+            else:
+                q = str(input('Сам разберешься с этим! Еще вопросы?'))
+    except KeyboardInterrupt:
+        print("\nСчастливо оставаться!")
 answer = str(input('Привет! Как жизнь?   '))
 get_answer(answer)
-question = str(input('Спрашивай о че хотел:)   '))
+question = str(input('Спрашивай о чем хотел:)   '))
 user_ask(question)
